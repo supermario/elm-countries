c30acd07946d81598af5118ef37c6d745f8412ed
Mario Rogic <hello@mario.net.au>
2018-10-06 22:37:21 +0100

    elm-format-0.19 0.8.0

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -1,76 +1,76 @@
 module Main exposing (main)
 
 import Browser
+import Countries exposing (Country)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Countries exposing (Country)
 
 
 type alias Model =
-  { searchString : String
-  , selectedCountry : Maybe Country
-  }
+    { searchString : String
+    , selectedCountry : Maybe Country
+    }
 
 
 type Msg
-  = SelectCountry Country
-  | UpdatePrefix String
+    = SelectCountry Country
+    | UpdatePrefix String
 
 
 init : Model
 init =
-  { searchString = ""
-  , selectedCountry = Nothing
-  }
+    { searchString = ""
+    , selectedCountry = Nothing
+    }
 
 
 main : Program () Model Msg
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    SelectCountry selected ->
-      { model | selectedCountry = Just selected }
+    case msg of
+        SelectCountry selected ->
+            { model | selectedCountry = Just selected }
 
-    UpdatePrefix prefix ->
-      { model | searchString = prefix }
+        UpdatePrefix prefix ->
+            { model | searchString = prefix }
 
 
 view : Model -> Html Msg
 view model =
-  let
-    selectedCountryString =
-      case model.selectedCountry of
-        Just country ->
-          country.name ++ " " ++ country.flag
+    let
+        selectedCountryString =
+            case model.selectedCountry of
+                Just country ->
+                    country.name ++ " " ++ country.flag
 
-        Nothing ->
-          "No country selected"
-  in
+                Nothing ->
+                    "No country selected"
+    in
     div
-      []
-      [ input [ type_ "text", onInput UpdatePrefix, placeholder "Search for a country" ] []
-      , h2 [] [ text selectedCountryString ]
-      , viewCountryList model.searchString
-      ]
+        []
+        [ input [ type_ "text", onInput UpdatePrefix, placeholder "Search for a country" ] []
+        , h2 [] [ text selectedCountryString ]
+        , viewCountryList model.searchString
+        ]
 
 
 viewCountryList : String -> Html Msg
 viewCountryList searchString =
-  div [] <|
-    List.map viewCountry (Countries.search searchString)
+    div [] <|
+        List.map viewCountry (Countries.search searchString)
 
 
 viewCountry : Country -> Html Msg
 viewCountry country =
-  div
-    [ onClick <| SelectCountry country ]
-    [ text <| country.flag ++ " " ++ country.name ++ " " ++ "(" ++ country.code ++ ")" ]
+    div
+        [ onClick <| SelectCountry country ]
+        [ text <| country.flag ++ " " ++ country.name ++ " " ++ "(" ++ country.code ++ ")" ]
--- src/Countries.elm
+++ src/Countries.elm
@@ -1,4 +1,8 @@
-module Countries exposing (Country, all, fromCode, search)
+module Countries exposing
+    ( Country
+    , fromCode, search
+    , all
+    )
 
 {-| A searchable [ISO 3166-1](https://en.wikipedia.org/wiki/ISO_3166-1) based list of country names, codes and emoji flags.
 
@@ -48,6 +52,7 @@ fromCode : String -> Maybe Country
 fromCode code =
     if String.length code /= 2 then
         Nothing
+
     else
         all
             |> List.filter
